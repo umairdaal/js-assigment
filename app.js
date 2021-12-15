4b142ca2835f54cf87e6e030300f631d84aa58b1
usmansiddqiui12321 <usmansiddqiui12321@gmail.com>
2021-12-15 21:06:03 +0500

    assignment completed

---
+++ app.js
@@ -0,0 +1,36 @@
+// var firstname = prompt("enter your first name");
+// var lastname = prompt("enter your last name");
+// var fullname = firstname + lastname;
+// console.log(fullname);
+
+// var a = prompt("Enter Your Favourat Mobile Name");
+// console.log(a);
+// String = input("");
+
+// var mobile = prompt("Enter Your Favourite Mobile Name");
+// var charsInmobile = mobile.length;
+// console.log(charsInmobile);
+
+// var arr = "pakistani";
+// console.log(arr[7]);
+
+// var arr = "hello worlld";
+// console.log(arr[9]);
+
+// var arr = "pakistani";
+// console.log(arr[3]);
+
+// var FirstName = "M.Umair";
+// var LastName = "M.Umer";
+// var FullName = FirstName + LastName;
+
+// console.log(FullName);
+
+// var arr = ["Hyderabad"];
+// arr[0] = "Islamabad";
+// console.log(arr);
+
+// var a = "peanut";
+// var b = a.toUpperCase();
+
+// console.log(b);
